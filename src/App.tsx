6d8a98894478452591fd14ec112a6215f006fceb
Dylan Sng <dsja612@gmail.com>
2023-07-11 15:59:13 +0800

    Removed unused files

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,4 @@
-import React from 'react';
-import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from "./components/Home";
-import Experience from './components/Experience';
-import Projects from './components/Projects';
 
 function App() {
   return (
